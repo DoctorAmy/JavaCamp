a05a471595237fa12a06e22ea2a59ee0be286a9e
MasterChao <631616788@qq.com>
2022-09-10 01:29:32 +0800

    init java camp project

---
+++ JavaCamp/src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    /*主函数*/
+    public static void main(String[] args){
+        helloWorld();
+    }
+
+    /*打印函数*/
+    public static void helloWorld() {
+        System.out.println("Hello World Go Go Go");
+        System.out.println("你好世界!");
+    }
+}
