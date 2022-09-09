51ec405584b6403b9f46ae8da85877a198b018b5
MasterChao <631616788@qq.com>
2022-09-10 01:37:54 +0800

    Create BigNumber.java
    基本数据类型前菜

---
+++ JavaCamp/src/BigNumber.java
@@ -0,0 +1,16 @@
+public class BigNumber {
+    public static void main(String[] args) {
+        int a = 100;
+        System.out.println(a);
+
+        int b = 1000000;
+        System.out.println(b);
+
+        // 整数太大
+        // int c = 100000000;
+        // System.out.println(c);
+
+        long d = 100000000;
+        System.out.println(d);
+    }
+}
